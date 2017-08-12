9d5237e427c0aa275f8488332184961e9a44426f
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-08-12 14:24:14 +0100

    Autocomplete refactored and route showing on map

---
+++ client/src/autoCompleteDirectionsHandler.js
@@ -0,0 +1,82 @@
+var AutocompleteDirectionsHandler = function(map) {
+  this.map = map;
+  this.originPlaceId = null;
+  this.destinationPlaceId = null;
+  this.travelMode = 'WALKING';
+  var originInput = document.getElementById('start-input');
+  var destinationInput = document.getElementById('end-input');
+  var modeSelector = document.getElementById('mode-selector');
+
+  this.directionsService = new google.maps.DirectionsService;
+  this.directionsDisplay = new google.maps.DirectionsRenderer;
+  this.directionsDisplay.setMap(map);
+
+  var originAutocomplete = new google.maps.places.Autocomplete(
+    originInput, {placeIdOnly: true});
+  var destinationAutocomplete = new google.maps.places.Autocomplete(
+    destinationInput, {placeIdOnly: true});
+
+  this.setupClickListener('changemode-walking', 'WALKING');
+  this.setupClickListener('changemode-bicycling', 'BICYCLING');
+
+
+  this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+  this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+  // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+  // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+  // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+}
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
+    var radioButton = document.getElementById(id);
+    var me = this;
+    radioButton.addEventListener('click', function() {
+      me.travelMode = mode;
+      me.route();
+    });
+  };
+
+  AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+    var me = this;
+    autocomplete.bindTo('bounds', this.map);
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+      if (!place.place_id) {
+        window.alert("Please select an option from the dropdown list.");
+        return;
+      }
+      if (mode === 'ORIG') {
+        me.originPlaceId = place.place_id;
+      } else {
+        me.destinationPlaceId = place.place_id;
+      }
+      me.route();
+    });
+
+  };
+
+  AutocompleteDirectionsHandler.prototype.route = function() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+      return;
+    }
+    var me = this;
+
+    this.directionsService.route({
+      origin: {'placeId': this.originPlaceId},
+      destination: {'placeId': this.destinationPlaceId},
+      travelMode: this.travelMode
+    }, function(response, status) {
+      if (status === 'OK') {
+        me.directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+
+  }
+
+
+  module.exports = AutocompleteDirectionsHandler;
--- client/src/entry.js
+++ client/src/entry.js
@@ -1,17 +1,28 @@
 var MapWrapper = require('./mapWrapper.js')
-var AutoComplete = require('./autoComplete.js')
+var AutoCompleteDirectionsHandler = require('./autoCompleteDirectionsHandler.js')
 var BikeOrHike = require('./bikeOrHike.js')
 var routeSelected = function(evt) {
   console.log("routeSelected");
 }
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('main-map'), {
+    mapTypeControl: false,
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13
+  });
+
+  new AutoCompleteDirectionsHandler(map);
+}
 
 var entry = function(){
+  initMap();
   // console.log('entry.js is working')
-  var mapDiv = document.querySelector("#main-map");
-  var center = {lat: 55.9470, lng: -3.2020}
-  var mainMap = new MapWrapper(mapDiv, center, 10);
+  // var mapDiv = document.querySelector("#main-map");
+  // var center = {lat: 55.9470, lng: -3.2020}
+  // var mainMap = new MapWrapper(mapDiv, center, 10);
   // mainMap.addMarker(center);
+ 
 
   var formSubmission = document.querySelector("#submit");
   formSubmission.addEventListener('click', routeSelected);
@@ -19,7 +30,7 @@ var entry = function(){
 
 
   // var travelMode = new BikeOrHike()
-  var auto = new AutoComplete(mainMap)
+  // var auto = new AutoCompleteDirectionsHandler(mainMap)
 }
 
 window.addEventListener('load', entry);
