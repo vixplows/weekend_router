d3a87063e900cd6e3b8cc99ecbeab0269e0d48dc
vixplows <vicky@vickyplows.co.uk>
2017-08-12 15:15:21 +0100

    Refactored code to be clean and deleted redundant files

--- client/src/entry.js
+++ client/src/entry.js
@@ -1,6 +1,4 @@
-var MapWrapper = require('./mapWrapper.js')
 var AutoCompleteDirectionsHandler = require('./autoCompleteDirectionsHandler.js')
-var BikeOrHike = require('./bikeOrHike.js')
 var routeSelected = function(evt) {
   console.log("routeSelected");
 }
@@ -17,20 +15,9 @@ function initMap() {
 
 var entry = function(){
   initMap();
-  // console.log('entry.js is working')
-  // var mapDiv = document.querySelector("#main-map");
-  // var center = {lat: 55.9470, lng: -3.2020}
-  // var mainMap = new MapWrapper(mapDiv, center, 10);
-  // mainMap.addMarker(center);
- 
 
   var formSubmission = document.querySelector("#submit");
   formSubmission.addEventListener('click', routeSelected);
-
-
-
-  // var travelMode = new BikeOrHike()
-  // var auto = new AutoCompleteDirectionsHandler(mainMap)
 }
 
 window.addEventListener('load', entry);
