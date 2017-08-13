c818b08d75497a133bf5710bd2eb3841b516db39
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-08-13 13:34:18 +0100

    connecting to database

--- server.js
+++ server.js
@@ -6,39 +6,38 @@ var path = require('path');
 var MongoClient = require('mongodb').MongoClient;
 var db;
 
-entry.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-
-entry.post('/route', function(req, res) {
-  res.redirect('/');
-});
-
 entry.use(parser.json());
 entry.use(parser.urlencoded({extended: true}));
 entry.use(express.static('client/build'));
 entry.use(express.static('client/public'));
 
-var server = entry.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('entry listening at http://%s:%s', host, port);
+entry.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-// MongoClient.connect('mongodb://localhost:27017/weekend_router'), function(err, database) {
+entry.post('/routes', function(req, res) {
+  db.collection('routes').save(req.body, function(err, result){
+    res.json(result);
+  });
+  // res.redirect('/');
+  // console.log("hello")
+});
+
+
+
+MongoClient.connect('mongodb://localhost:27017/weekend_router', function(err, database) {
   
-//   if(err) {
-//     console.log(err);
-//     return;
-//   }
+  if(err) {
+    console.log(err);
+    return;
+  }
 
-//   db = database;
+  db = database;
 
-//   console.log('Connected to database');
+  console.log('Connected to database');
 
-//   entry.listen(3000, function() {
-//     console.log('Listening on port 3000');
-//   });
-
-// }
+  entry.listen(3000, function() {
+    console.log('Listening on port 3000');
+  });
+});
