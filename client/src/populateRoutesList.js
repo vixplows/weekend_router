366a4ce4ebb8987b04ec5dc2090e78b7b005999b
vixplows <vicky@vickyplows.co.uk>
2017-08-14 22:53:52 +0100

    Notes in files to discuss with team in morning review

--- client/src/populateRoutesList.js
+++ client/src/populateRoutesList.js
@@ -73,6 +73,7 @@ PopulateRoutesList.prototype.render = function (routes) {
       // when clicked it should:
       // take start and end points (place ids? might need lat and lngs) and the travel mode and give to a route directions object/function to then display on map
 
+      // hard coded to test that map changes when a route name is clicked - if attach lat lng to nameLi element then could access dynamically here but would still need a way of setting both start and end, travel mode and route
       var map = new google.maps.Map(document.getElementById('main-map'), {
         mapTypeControl: false,
         center: {lat: 53.8, lng: -1.54},
