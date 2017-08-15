5ec36ee34ff179461d2e831262bde57a7137b514
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-08-15 17:13:13 +0100

    markers added for set location

--- client/src/entry.js
+++ client/src/entry.js
@@ -1,5 +1,56 @@
 var AutoCompleteDirectionsHandler = require('./autoCompleteDirectionsHandler.js');
 var PopulateRoutesList = require('./PopulateRoutesList');
+// var PubFinder = require('./PubFinder');
+
+///
+
+var displayPubs = function() {
+
+  // var map = new google.maps.Map(document.getElementById('main-map'); 
+  var map;
+  var infowindow;
+
+// function initMap() {
+  console.log('I have arrived')
+
+    var codeclan = {lat: 55.9470 , lng: -3.2020};
+
+    map = new google.maps.Map(document.getElementById('main-map'), {
+      center: codeclan,
+      zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: codeclan,
+      radius: 500,
+      type: ['bar']
+    }, callback);
+  // }
+
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+}
+///
 
 var routeSelected = function(evt, callback) {
   var startInput = document.querySelector("#start-input");
@@ -84,6 +135,12 @@ var entry = function(){
   var saveRoute = document.querySelector("#save-route");
   saveRoute.addEventListener('click', function() {
     routeSelected(this.responseText, requestComplete);
+  });
+
+//not sure if should be response text
+  var showPubs = document.querySelector("#show-pubs");
+  showPubs.addEventListener('click', function() {
+    displayPubs();
   });
 };
 
